16f20e42e39c517cf5719b504cf3cdac8d468ea9
Calvin Gunther <calvingunther66@gmail.com>
2026-03-03 13:13:42 -0800

    refactor: move UART RX line processing from IRQ to app tick using a stream buffer and increase tick rate

--- flipper_app/fap_retrosynth.c
+++ flipper_app/fap_retrosynth.c
@@ -1,5 +1,4 @@
 #include <dialogs/dialogs.h>
-#include <expansion/expansion.h>
 #include <furi.h>
 #include <furi_hal.h>
 #include <furi_hal_serial.h>
@@ -14,6 +13,8 @@
 #include <stdlib.h>
 #include <storage/storage.h>
 #include <string.h>
+#include <expansion/expansion.h>
+#include <core/stream_buffer.h>
 
 #define BAUD_RATE 115200
 #define MAX_DRAW_LINES 120
@@ -43,6 +44,7 @@ typedef struct {
   Widget *widget;
 
   FuriHalSerialHandle *serial_handle;
+  FuriStreamBuffer *rx_stream;
 
   char smiles_buffer[256];
   char result_buffer[128];
@@ -98,14 +100,7 @@ static void uart_on_irq_cb(FuriHalSerialHandle *handle,
     MolRetroApp *app = context;
     while (furi_hal_serial_async_rx_available(handle)) {
       uint8_t data = furi_hal_serial_async_rx(handle);
-      if (data == '\n') {
-        app->rx_line_buffer[app->rx_line_idx] = '\0';
-        parse_uart_line(app, app->rx_line_buffer);
-        app->rx_line_idx = 0;
-      } else if (data != '\r' &&
-                 app->rx_line_idx < (int)sizeof(app->rx_line_buffer) - 1) {
-        app->rx_line_buffer[app->rx_line_idx++] = data;
-      }
+      furi_stream_buffer_send(app->rx_stream, &data, 1, 0);
     }
   }
 }
@@ -115,7 +110,6 @@ static void submenu_callback(void *context, uint32_t index) {
   if (index == MolRetroEventSubmenuText) {
     view_dispatcher_switch_to_view(app->view_dispatcher, MolRetroViewTextInput);
   } else if (index == MolRetroEventSubmenuFile) {
-    // Run Dialogs Browser
     FuriString *file_path = furi_string_alloc();
     DialogsApp *dialogs = furi_record_open(RECORD_DIALOGS);
 
@@ -128,7 +122,6 @@ static void submenu_callback(void *context, uint32_t index) {
     furi_record_close(RECORD_DIALOGS);
 
     if (res) {
-      // Read File
       Storage *storage = furi_record_open(RECORD_STORAGE);
       File *file = storage_file_alloc(storage);
 
@@ -138,8 +131,6 @@ static void submenu_callback(void *context, uint32_t index) {
         storage_file_read(file, app->smiles_buffer,
                           sizeof(app->smiles_buffer) - 1);
 
-        // Replace any newline with null terminator so we just get the first
-        // line SMILES
         for (size_t i = 0; i < strlen(app->smiles_buffer); i++) {
           if (app->smiles_buffer[i] == '\n' || app->smiles_buffer[i] == '\r') {
             app->smiles_buffer[i] = '\0';
@@ -148,7 +139,6 @@ static void submenu_callback(void *context, uint32_t index) {
         }
 
         storage_file_close(file);
-        // Trigger transition
         trigger_drawing(app);
       }
       storage_file_free(file);
@@ -171,13 +161,11 @@ static void canvas_draw_cb(Canvas *canvas, void *context) {
     canvas_draw_str_aligned(canvas, 64, 32, AlignCenter, AlignCenter,
                             "Generating 2D Layout...");
   } else {
-    // Draw the molecule lines
     for (int i = 0; i < app->num_lines; ++i) {
       canvas_draw_line(canvas, app->lines[i].x1, app->lines[i].y1,
                        app->lines[i].x2, app->lines[i].y2);
     }
 
-    // Instruction Box
     canvas_set_color(canvas, ColorWhite);
     canvas_draw_box(canvas, 0, 0, 128, 12);
     canvas_set_color(canvas, ColorBlack);
@@ -190,11 +178,9 @@ static bool canvas_input_cb(InputEvent *event, void *context) {
   MolRetroApp *app = context;
   if (event->type == InputTypeShort && event->key == InputKeyOk) {
     if (!app->is_drawing && app->num_lines > 0) {
-      // Trigger Retrosynthesis
       app->is_analyzing = true;
       memset(app->result_buffer, 0, sizeof(app->result_buffer));
 
-      // Send RETRO: command
       char cmd[300];
       snprintf(cmd, sizeof(cmd), "RETRO:%s\n", app->smiles_buffer);
       furi_hal_serial_tx(app->serial_handle, (uint8_t *)cmd, strlen(cmd));
@@ -232,6 +218,19 @@ static uint32_t widget_prev_callback(void *context) {
 
 static void app_tick(void *context) {
   MolRetroApp *app = context;
+  
+  uint8_t data;
+  while(furi_stream_buffer_receive(app->rx_stream, &data, 1, 0) == 1) {
+    if (data == '\n') {
+      app->rx_line_buffer[app->rx_line_idx] = '\0';
+      parse_uart_line(app, app->rx_line_buffer);
+      app->rx_line_idx = 0;
+    } else if (data != '\r' &&
+               app->rx_line_idx < (int)sizeof(app->rx_line_buffer) - 1) {
+      app->rx_line_buffer[app->rx_line_idx++] = data;
+    }
+  }
+
   if (app->is_analyzing == false && strlen(app->result_buffer) > 0) {
     widget_reset(app->widget);
     widget_add_string_element(app->widget, 2, 10, AlignLeft, AlignTop,
@@ -240,7 +239,6 @@ static void app_tick(void *context) {
                               FontSecondary, "Template Class Predicted:");
     widget_add_string_multiline_element(app->widget, 2, 45, AlignLeft, AlignTop,
                                         FontPrimary, app->result_buffer);
-    // Clear buffer so we only update the widget once
     memset(app->result_buffer, 0, sizeof(app->result_buffer));
   }
 }
@@ -250,15 +248,16 @@ int32_t mol_retro_app(void *p) {
   MolRetroApp *app = malloc(sizeof(MolRetroApp));
   memset(app, 0, sizeof(MolRetroApp));
 
+  app->rx_stream = furi_stream_buffer_alloc(512, 1);
+
   app->gui = furi_record_open(RECORD_GUI);
   app->view_dispatcher = view_dispatcher_alloc();
   view_dispatcher_attach_to_gui(app->view_dispatcher, app->gui,
                                 ViewDispatcherTypeFullscreen);
 
-  view_dispatcher_set_tick_event_callback(app->view_dispatcher, app_tick, 500);
+  view_dispatcher_set_tick_event_callback(app->view_dispatcher, app_tick, 50);
   view_dispatcher_set_event_callback_context(app->view_dispatcher, app);
 
-  // 0. Submenu View
   app->submenu = submenu_alloc();
   submenu_add_item(app->submenu, "Enter SMILES", MolRetroEventSubmenuText,
                    submenu_callback, app);
@@ -267,7 +266,6 @@ int32_t mol_retro_app(void *p) {
   view_dispatcher_add_view(app->view_dispatcher, MolRetroViewSubmenu,
                            submenu_get_view(app->submenu));
 
-  // 1. Text Input View
   app->text_input = text_input_alloc();
   text_input_set_header_text(app->text_input, "Enter SMILES String:");
   text_input_set_result_callback(app->text_input, text_input_callback, app,
@@ -278,7 +276,6 @@ int32_t mol_retro_app(void *p) {
   view_dispatcher_add_view(app->view_dispatcher, MolRetroViewTextInput,
                            text_input_get_view(app->text_input));
 
-  // 2. 2D Molecule Canvas View
   app->canvas_view = view_alloc();
   view_set_context(app->canvas_view, app);
   view_set_draw_callback(app->canvas_view, canvas_draw_cb);
@@ -287,31 +284,25 @@ int32_t mol_retro_app(void *p) {
   view_dispatcher_add_view(app->view_dispatcher, MolRetroViewCanvas,
                            app->canvas_view);
 
-  // 3. Results Widget View
   app->widget = widget_alloc();
   view_set_previous_callback(widget_get_view(app->widget),
                              widget_prev_callback);
   view_dispatcher_add_view(app->view_dispatcher, MolRetroViewWidget,
                            widget_get_view(app->widget));
 
-  // Modern FuriHalSerial initialization
-  // We MUST disable the expansion service first, otherwise FuriHalSerialIdUsart
-  // is busy and returns NULL!
   Expansion *expansion = furi_record_open(RECORD_EXPANSION);
   expansion_disable(expansion);
 
   app->serial_handle = furi_hal_serial_control_acquire(FuriHalSerialIdUsart);
-  furi_assert(app->serial_handle); // Graceful crash if it still fails
+  furi_assert(app->serial_handle); 
 
   furi_hal_serial_init(app->serial_handle, BAUD_RATE);
   furi_hal_serial_async_rx_start(app->serial_handle, uart_on_irq_cb, app,
                                  false);
 
-  // Initial View
   view_dispatcher_switch_to_view(app->view_dispatcher, MolRetroViewSubmenu);
   view_dispatcher_run(app->view_dispatcher);
 
-  // Cleanup
   furi_hal_serial_async_rx_stop(app->serial_handle);
   furi_hal_serial_deinit(app->serial_handle);
   furi_hal_serial_control_release(app->serial_handle);
@@ -329,6 +320,8 @@ int32_t mol_retro_app(void *p) {
   widget_free(app->widget);
   view_dispatcher_free(app->view_dispatcher);
   furi_record_close(RECORD_GUI);
+  
+  furi_stream_buffer_free(app->rx_stream);
   free(app);
 
   return 0;
